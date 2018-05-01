5c196be9d3384456aa2d54b12f4dca9f9a2310fb
杨新磊 <yangxinlei@yangxinleideMacBook-Pro.local>
2018-05-01 22:42:56 +0800

    no message

---
+++ JGCALayer/JGCALayer/Apps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JGCALayer
+//
+//  Created by stkcctv on 16/9/11.
+//  Copyright © 2016年 stkcctv. All rights reserved.
+//测试测试
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
